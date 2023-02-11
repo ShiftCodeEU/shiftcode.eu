b064b6cca93af0bd0982f0b864b446bee1625bf5
JesseKoldewijn <dev@glitchtech.eu>
2023-02-11 22:16:47 +0100

    update: translation namespacing added

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
-  defaultLocale: "en",
-  locales: [
-    "en", // The `defaultLocale` value must present in `locales` keys
-    "de",
-    "fr",
-    "es",
-    "nl",
-  ],
+	defaultNamespace: "common",
+	namespaces: ["common", "pages", "errors"],
+	defaultLocale: "en",
+	locales: [
+		"en", // The `defaultLocale` value must present in `locales` keys
+		"de",
+		"fr",
+		"es",
+		"nl"
+	]
 };
