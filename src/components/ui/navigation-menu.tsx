b8987d6057242aa8bd6bc74144516abad504c044
Jesse Koldewijn <jesse@iungo.nl>
2023-03-16 16:57:37 +0100

    fix: small nav styling tweak

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -41,7 +41,7 @@ NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName;
 const NavigationMenuItem = NavigationMenuPrimitive.Item;
 
 const navigationMenuTriggerStyle = cva(
-	"inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:bg-accent disabled:opacity-50 disabled:pointer-events-none bg-secondary hover:bg-accent text-white hover:text-accentAlt data-[state=open]:bg-accent dark:data-[state=open]:bg-accent data-[active]:bg-accent h-10 py-2 px-4 group w-max"
+	"inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:bg-accent disabled:opacity-50 disabled:pointer-events-none bg-secondary hover:bg-accent text-black hover:text-white data-[state=open]:bg-accent data-[active]:bg-accent h-10 py-2 px-4 group w-max"
 );
 
 const NavigationMenuTrigger = React.forwardRef<
